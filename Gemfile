0ce0050e579a50da455610ce4d01c85706f038e9
mikegee <gee.24@osu.edu>
2012-05-16 14:17:03 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+
+gem 'sqlite3'
+
+gem 'capistrano'
+gem 'json'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'therubyracer', :platform => :ruby
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthShibTester::Application.config.secret_token = 'a6e2a657c794aa33b52e5267ff7d4057f7533e7a1d87ae0754903918dd05c38e1ae4218b0dc4de2a321264f5aa18036d65aba99cd02159a5cde2fcfa546af77c'
